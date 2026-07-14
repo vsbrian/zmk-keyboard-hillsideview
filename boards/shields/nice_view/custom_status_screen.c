e5e92c5b5370b8aed14fb4f38139ff357ec245e3
vsbrian <127261808+vsbrian@users.noreply.github.com>
2026-07-14 16:56:11 -0500

    Add files via upload

--- boards/shields/nice_view/custom_status_screen.c
+++ boards/shields/nice_view/custom_status_screen.c
@@ -1,27 +1,28 @@
-# nice!view
+/*
+ *
+ * Copyright (c) 2023 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ *
+ */
 
-The nice!view is a low-power, high refresh rate display meant to replace I2C OLEDs traditionally used.
+#include "widgets/status.h"
 
-This shield requires that an `&nice_view_spi` labeled SPI bus is provided with _at least_ MOSI, SCK, and CS pins defined.
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-## Custom widget
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
+static struct zmk_widget_status status_widget;
+#endif
 
-The nice!view shield includes a custom vertical widget.
+lv_obj_t *zmk_display_status_screen() {
 
-Profile indicators show the status of the first five BLE profiles using numbers from 1 to 5.
-The number corresponding to the currently selected profile is drawn in a filled disk
-and the circle outline around each profile number correspond to the following states:
+    lv_obj_t *screen;
+    screen = lv_obj_create(NULL);
 
-- solid outline: connected
-- dashed outline: not connected
-- no outline: not bound
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
+    zmk_widget_status_init(&status_widget, screen);
+    lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
 
-## Disable custom widget
-
-To use the built-in ZMK widget instead of the custom nice!view one, add the following item to your `.conf` file:
-
-```
-CONFIG_ZMK_DISPLAY_STATUS_SCREEN_BUILT_IN=y
-CONFIG_ZMK_LV_FONT_DEFAULT_SMALL_MONTSERRAT_26=y
-CONFIG_LV_FONT_DEFAULT_MONTSERRAT_26=y
-```
+    return screen;
+}
