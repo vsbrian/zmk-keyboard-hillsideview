586891684faff379be21abb334b49ef791dee694
vsbrian <127261808+vsbrian@users.noreply.github.com>
2026-07-14 16:54:46 -0500

    Update util.h

--- boards/shields/nice_view/widgets/util.h
+++ boards/shields/nice_view/widgets/util.h
@@ -1,24 +1,51 @@
 /*
  *
- * Copyright (c) 2023 The ZMK Contributors
+ * Copyright (c) 2025 The ZMK Contributors
  * SPDX-License-Identifier: MIT
  *
  */
 
-#pragma once
-
 #include <lvgl.h>
-#include <zephyr/kernel.h>
-#include "util.h"
+#include <zmk/endpoints.h>
 
-struct zmk_widget_status {
-    sys_snode_t node;
-    lv_obj_t *obj;
-    lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
-    lv_color_t cbuf2[CANVAS_SIZE * CANVAS_SIZE];
-    lv_color_t cbuf3[CANVAS_SIZE * CANVAS_SIZE];
-    struct status_state state;
+#define NICEVIEW_PROFILE_COUNT 2
+
+#define CANVAS_SIZE 68
+
+#define LVGL_BACKGROUND                                                                            \
+    IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
+#define LVGL_FOREGROUND                                                                            \
+    IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_white() : lv_color_black()
+
+struct status_state {
+    uint8_t battery;
+    bool charging;
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    struct zmk_endpoint_instance selected_endpoint;
+    int active_profile_index;
+    bool active_profile_connected;
+    bool active_profile_bonded;
+    bool profiles_connected[NICEVIEW_PROFILE_COUNT];
+    bool profiles_bonded[NICEVIEW_PROFILE_COUNT];
+    uint8_t layer_index;
+    const char *layer_label;
+    uint8_t wpm[10];
+#else
+    bool connected;
+#endif
 };
 
-int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget);
+struct battery_status_state {
+    uint8_t level;
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+    bool usb_present;
+#endif
+};
+
+void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]);
+void draw_battery(lv_obj_t *canvas, const struct status_state *state);
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
+                    lv_text_align_t align);
+void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color);
+void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width);
+void init_arc_dsc(lv_draw_arc_dsc_t *arc_dsc, lv_color_t color, uint8_t width);
